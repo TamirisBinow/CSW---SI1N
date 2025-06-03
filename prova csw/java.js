7a7089774613875c35b0bc52c9e674f538524861
TamirisBinow <tamiris.binow@gmail.com>
2025-06-03 13:46:23 -0300

    Add files via upload

---
+++ prova csw/java.js
@@ -0,0 +1,7 @@
+function enviar(){
+
+    var num1 = document.getElementById('num1').value;
+    var ano = 2025;
+    var resultado = 2025 - num1;
+    document.getElementById("resultado").textContent = "Resultado: " + resultado;
+}
